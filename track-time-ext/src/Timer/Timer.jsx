5b02c48811285c18590664d90a9e409682796f19
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-09-01 18:25:51 +0530

    Ui and reports router

---
+++ track-time-ext/src/Timer/Timer.jsx
@@ -0,0 +1,30 @@
+import { Box, Button, Center, Flex, Stack, Text } from '@chakra-ui/react'
+import React from 'react'
+import {
+Link
+  } from "react-router-dom";
+const Timer = () => {
+  return (
+    <Center>
+        <Stack gap={8}>
+
+        <Box m={10} w={40}  border={'5px double '} borderColor={'gray.300'} rounded={8}>
+            <Text p={4} textAlign={'center'} fontSize={20} bg={'blackAlpha.600'} color={'white'}>
+
+            11:11:11
+            </Text>
+        </Box>
+        <Flex gap={2}>
+            <Button bg={'green.400'} color={'white'}>Start</Button>
+            <Button bg={'orange.400'} color={'white'}>Break</Button>
+            <Button bg={'red.400'} color={'white'}>Leave</Button>
+        </Flex>
+        <Link to={'/reports'}>
+<Button w={'100%'} bg={'blue.300'} color={'white'}>Reports</Button>
+        </Link>
+        </Stack>
+    </Center>
+  )
+}
+
+export default Timer
